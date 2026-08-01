abba2ea3c0c41d268bc5b6e3ac70dd930d7cd801
Axe_Raider999 <axeraider999@gmail.com>
2026-08-01 01:52:29 -0700

    Reflection

--- reflection.py
+++ reflection.py
@@ -8,27 +8,50 @@ grid = [input() for _ in range(n)]
 
 half = n // 2
 
-# --- line up the mirror groups -----------------------------------------
-# n//2 iterations, so each group is visited exactly once.
-for r in range(half):
-    top = grid[r]
-    bot = grid[n - 1 - r]
 
-    # Reversing the right halves makes index c line up with column n-1-c,
-    # so position c of all four strings is one set of mirror partners.
-    top_left = top[:half]
-    top_right = top[half:][::-1]
-    bot_left = bot[:half]
-    bot_right = bot[half:][::-1]
+def scan():
+    total = 0
 
-    for c in range(half):
-        tl = top_left[c]    # row r,     col c
-        tr = top_right[c]   # row r,     col n-1-c
-        bl = bot_left[c]    # row n-1-r, col c
-        br = bot_right[c]   # row n-1-r, col n-1-c
+    # --- line up the mirror groups -------------------------------------
+    # n//2 iterations, so each group is visited exactly once.
+    for r in range(half):
+        top = grid[r]
+        bot = grid[n - 1 - r]
 
-        # Temporary - check the alignment against the sample by hand,
-        # then delete this and put your counting logic here.
-        print(r, c, tl, tr, bl, br)
+        # Reversing the right halves makes index c line up with column n-1-c,
+        # so position c of all four strings is one set of mirror partners.
+        top_left = top[:half]
+        top_right = top[half:][::-1]
+        bot_left = bot[:half]
+        bot_right = bot[half:][::-1]
 
-# --- U section: later ---------------------------------------------------
+        for c in range(half):
+            tl = top_left[c]    # row r,     col c
+            tr = top_right[c]   # row r,     col n-1-c
+            bl = bot_left[c]    # row n-1-r, col c
+            br = bot_right[c]   # row n-1-r, col n-1-c
+
+            hashes = [tl, tr, bl, br].count("#")
+
+            if hashes == 1 or hashes == 3:
+                total += 1
+            elif hashes == 2:
+                total += 2
+
+    return total
+
+
+# --- U section ----------------------------------------------------------
+print(scan())
+
+for _ in range(u):
+    r, c = map(int, input().split())
+    r -= 1
+    c -= 1
+
+    if grid[r][c] == "#":
+        grid[r] = grid[r][:c] + "." + grid[r][c + 1:]
+    else:
+        grid[r] = grid[r][:c] + "#" + grid[r][c + 1:]
+
+    print(scan())
